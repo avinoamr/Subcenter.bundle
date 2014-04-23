aa25cf066118a64816ef67a205b81a5d22b06043
Roi Avinoam <avinoamr@gmail.com>
2014-04-23 13:24:41 +0300

    Add several heuristics to improve the search quality

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,21 +1,38 @@
-# import urllib2
-# from lxml import etree
-# import json
-# class HTML:
-#     @staticmethod
-#     def ElementFromURL( url, *args, **kwargs ):
-#         return etree.HTML( urllib2.urlopen( url ).read() )
-# class HTTP:
-#     @staticmethod
-#     def Request( url, *args, **kwargs ):
-#         class HTTPRequest:
-#             content = urllib2.urlopen( url ).read()
-#         return HTTPRequest()
-# class JSON:
-#     @staticmethod
-#     def ObjectFromURL( url, *args, **kwargs ):
-#         return json.loads( urllib2.urlopen( url ).read() )
-# def Log( s ): print s
+import urllib2
+from lxml import etree
+import json
+class HTML:
+    @staticmethod
+    def ElementFromURL( url, *args, **kwargs ):
+        return etree.HTML( urllib2.urlopen( url ).read() )
+class HTTP:
+    @staticmethod
+    def Request( url, *args, **kwargs ):
+        class HTTPRequest:
+            content = urllib2.urlopen( url ).read()
+        return HTTPRequest()
+class JSON:
+    @staticmethod
+    def ObjectFromURL( url, *args, **kwargs ):
+        return json.loads( urllib2.urlopen( url ).read() )
+class Part:
+    subtitles = { "he": {} }
+    def __init__( self, f ): self.file = f
+class Language:
+    Hebrew = "he"
+    NoLanguage = ""
+class Locale:
+    Language = Language
+class Media:
+    def __init__( self, *args, **kwargs ): pass
+class Proxy:
+    Media = Media
+class TV_Shows: pass
+class Movies: pass
+class Agent:
+    TV_Shows = TV_Shows
+    Movies = Movies
+def Log( s ): print s
 
 
 ##
@@ -25,26 +42,48 @@ URL_SUBS = "http://subscenter.cinemast.com/he/cinemast/data/"
 URL_DOWNLOAD = "http://subscenter.cinemast.com/subtitle/download/he/"
 MAX_TITLES = 4 # maximum top search result titles to review
 MAX_RESULTS = 4 # maximum subtitles to download
+LEVENSHTEIN_MAX = 0.5 
 FORMATS = [ "hdtv", "480p", "720p", "1080p" ]
 
-def search( name, fname, fmt, season = None, episode = None ):
+def search( name, fname, season = None, episode = None ):
     Log( "Search for: " + name )
+
+    fname = fname.lower()
+    formats = filter( lambda f: f in fname, FORMATS )
+    if not formats: return Log( "Failed to find format for: " + fname )
+    fmt = formats[ 0 ]
+
     url = URL_SEARCH + "?q=" + name.lower().replace( " ", "+" )
     html = HTML.ElementFromURL( url, headers = { "User-Agent": HTTP_USER_AGENT } ) 
 
     titles = []
     for r in html.xpath( "//div[@id='processes']//a" ):
         if not r.text.strip(): continue
-        name = r.attrib[ "href" ].split( "/" )[ -2 ]
-        Log( "Search found: " + name )
-        titles.append( name )
+        title = r.attrib[ "href" ].split( "/" )[ -2 ]
+        Log( "Search found: " + title )
+        titles.append( title )
         if len( titles ) > MAX_TITLES: break # 
 
     subs = []
     for t in titles:
         subs += get( t, fmt, season, episode )
     Log( "Received %s (Total) subs for %s" % ( len( subs ), name ) )
-    subs = sorted( subs, cmpstr( fname ) )
+
+    # first, try to filter out the non-exact matches (assuming atleast one is found)
+    cfname = compact( fname )
+    exact = filter( lambda s: compact( s[ "subtitle_version"] ) == cfname, subs )
+    if exact: 
+        Log( "Exact match found: " + fname )
+        subs = exact
+
+    # filter out results that are too far away from the search string
+    l = lambda s: levenshtein( cfname, compact( s[ "subtitle_version" ] ) )
+    subs = filter( lambda s: l( s ) < LEVENSHTEIN_MAX, subs )
+
+    # sort by download count
+    subs = sorted( subs, None, lambda s: s[ "downloaded" ], True )
+
+    # return the maximum allowed results
     return subs[ :MAX_RESULTS ]
 
 
@@ -79,37 +118,45 @@ def download( sub ):
     url = URL_DOWNLOAD + "%s/?v=%s&key=%s" % ( sub[ "id" ], v, k )
     Log( "Downloading subtitle: " + url )
     try:
-        return HTTP.Request( url, headers = { "User-Agent": HTTP_USER_AGENT } ).content
+        sub = HTTP.Request( url, headers = { "User-Agent": HTTP_USER_AGENT } ).content
     except Exception as e:
         Log( "ERROR downloading: " + url )
         Log( e )
+    return sub
+
 
 def compact( s ):
     return s.lower().strip().replace( ".", "" ).replace( "-", "" ).replace( "_", "" )
 
-def cmpstr( name ):
-    name = compact( name )
-    def compare( s, t ):
-        if compact( s[ "subtitle_version" ] ) == name: return -1
-        elif compact( t[ "subtitle_version" ] ) == name: return 1
-        else: return cmp( s[ "downloaded" ], t[ "downloaded" ] )
-    return compare
+# compute the levenshtein distance between the two strings
+# recursive implementation with memoization for efficiency
+def levenshtein( s, t ):
+    if len( s ) == 0: return 1 # 100% difference
+    m = {}
+    def do( si, ti ):
+        if si == 0: return ti
+        if ti == 0: return si
+        if ( si, ti ) in m: return m[ si, ti ] # memoized
+
+        cost = 0 if s[ si - 1 ] == t[ ti - 1 ] else 1
+        m[ si, ti ] = min( 
+            do( si - 1, ti ) + 1, # deletion
+            do( si, ti - 1 ) + 1, # insertion
+            do( si - 1, ti - 1 ) + cost # substitution
+        )
+        return m[ si, ti ]
+    return do( len( s ), len( t ) ) / float( len( s ) ) # can't be zero division
 
 
 def update( part, name, season = None, episode = None ):
-    fname = part.file.lower()
-    formats = filter( lambda f: f in fname, FORMATS )
-    if not formats: 
-        return Log( "Failed to find format for: " + fname )
-
-    locale = Locale.Language.Unknown
-    subs = search( name, fname, formats[ 0 ], season, episode )
+    locale = Locale.Language.Hebrew
+    subs = search( name, part.file, season, episode )
     for sub in subs:
         v = sub[ "subtitle_version" ]
         sub = download( sub )
         if sub:
             part.subtitles[ locale ][ v ] = Proxy.Media( sub, ext = "srt" )
-
+            Log( "Subtitle saved for: " + v )
 
 class SubcenterTV( Agent.TV_Shows ):
     name = "Subcenter.org"
@@ -148,3 +195,6 @@ class SubcenterMovies( Agent.Movies ):
         for i in media.items:
             for part in i.parts:
                 update( part, media.title )
+
+
+# update( Part( "AboutTime.2013.720p.BrRip.x264.YIFY" ), "About Time" ) # test
